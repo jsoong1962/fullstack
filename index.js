56e8ec86be083835504acdd9b7626240584eb63b
github username <github email address>
2019-03-18 16:12:25 -0400

    added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser')
 require('./models/User'); //user Model first
 require('./services/passport');
 const keys = require('./config/keys');
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +25,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production') {
+  // express will serve up main.js / .css index.html from client in react
+  app.use(express.static('client/build'));
+  // if none are find ,  default to index
+  const path = require('path');
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
