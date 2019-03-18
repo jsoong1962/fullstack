dbd9c5b74febb15f8c281dfb9f43a6360160b5be
github username <github email address>
2019-03-18 16:40:35 -0400

    exposed build

---
+++ client/build/precache-manifest.43247113602cf8a978db30830125762b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3abb3a3b6a06f3fc296",
+    "url": "/static/js/main.fa954d19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d145ad52e4096912fbf6",
+    "url": "/static/css/2.f47abbc0.chunk.css"
+  },
+  {
+    "revision": "d145ad52e4096912fbf6",
+    "url": "/static/js/2.e4f13acc.chunk.js"
+  },
+  {
+    "revision": "d13e755df26da869d2085be093c357e9",
+    "url": "/index.html"
+  }
+];
